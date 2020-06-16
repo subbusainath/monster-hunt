98375c6c1f9f9248c94833804c3870a986dcebf5
subbusainath <subbusainath@outlook.com>
2020-06-16 17:11:16 +0530

    Updates

---
+++ precache-manifest.337c7e0527acd5bf0238003d4ee12033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3396a212c0ff92a33d3d940ec3df9892",
+    "url": "/monster-hunt/index.html"
+  },
+  {
+    "revision": "867cb9d33c2d04873378",
+    "url": "/monster-hunt/static/css/main.f1323f90.chunk.css"
+  },
+  {
+    "revision": "5a936a848308371c0003",
+    "url": "/monster-hunt/static/js/2.25a3f3bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-hunt/static/js/2.25a3f3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867cb9d33c2d04873378",
+    "url": "/monster-hunt/static/js/main.3b225ed3.chunk.js"
+  },
+  {
+    "revision": "a754cba3a3224214733d",
+    "url": "/monster-hunt/static/js/runtime-main.e21c8228.js"
+  }
+]);
